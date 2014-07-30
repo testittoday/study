b1eabe025e3eaf8db6872a26185bce11a5ad1d60
TianCaiBenBen <chao.zhan@hotmail.com>
2014-07-30 00:19:45 -0400

    In Permutation1.java, only do the swap if i is equal to j; adjusted indent as well

--- algorithm/src/main/java/zhan/Permutation1.java
+++ algorithm/src/main/java/zhan/Permutation1.java
@@ -12,45 +12,49 @@ package zhan;
  */
 public class Permutation1<T> {
 
-	public void printA(final T[] a) {
-		StringBuilder b = new StringBuilder();
+    public void printA(final T[] a) {
+        StringBuilder b = new StringBuilder();
 
-		for (T e : a) {
-			b.append(String.valueOf(e));
-		}
-		System.out.println(b.toString());
-	}
+        for (T e : a) {
+            b.append(String.valueOf(e));
+        }
+        System.out.println(b.toString());
+    }
 
-	public void swap(final T[] a, int x, int y) {
-		T temp = a[x];
-		a[x] = a[y];
-		a[y] = temp;
-	}
+    public void swap(final T[] a, int x, int y) {
+        T temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
 
-	public void permute(final int i, final T[] array) {
-		final int N = array.length - 1;
-		if (i == N) {
-			printA(array);
-		} else {
-			for (int j = i; j <= N; j++) {
-				swap(array, i, j);
-				permute(i + 1, array);
-				swap(array, i, j);
+    public void permute(final int i, final T[] array) {
+        final int N = array.length - 1;
+        if (i == N) {
+            printA(array);
+        } else {
+            for (int j = i; j <= N; j++) {
+                if (i != j) {
+                    swap(array, i, j);
+                }
+                permute(i + 1, array);
+                if (i != j) {
+                    swap(array, i, j);
+                }
 
-			}
-		}
+            }
+        }
 
-	}
+    }
 
-	public static void main(final String args[]) {
-		// String[] A = {"a", "b", "c", "d"};
-		// printA(A);
-		// swap(A, 0, 3);
-		// printA(A);
-		Permutation1<String> permutation = new Permutation1<String>();
-		String[] A = { "a", "b", "c" };
-		permutation.permute(0, A);
+    public static void main(final String args[]) {
+        // String[] A = {"a", "b", "c", "d"};
+        // printA(A);
+        // swap(A, 0, 3);
+        // printA(A);
+        Permutation1<String> permutation = new Permutation1<String>();
+        String[] A = { "a", "b", "c" };
+        permutation.permute(0, A);
 
-	}
+    }
 
 }
